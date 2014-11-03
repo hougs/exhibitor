fede8d13f044d9428acff8a60b545e0ba11bb9bd
Juliet Hougland <juliet@cloudera.com>
2014-11-02 22:58:56 -0800

    meh

--- app/models/Entity.scala
+++ app/models/Entity.scala
@@ -1,13 +1,27 @@
 package models
 
-import play.api.db.DB
+import java.sql.ResultSet
 
-case class Entity(id: Long, results:)
+import play.api.db.DB
+import play.api.
+import play.api.libs.json.Json
+import play.api.libs.json.JsValue
+import play.api.libs.json.Writes
+
+case class Entity(id: Long)
 
 object Entity {
-  implicit def Writes[Entity] =
+  implicit val entityWrites = new Writes[Entity] {
+    def writes(ent: Entity): JsValue = {
+      Json.obj(
+        "id" -> ent.id
+      )
+    }
+  }
+
   def getById(id: Long) = DB.withConnection { implicit conn =>
-      conn.
+      val statement = conn.prepareStatement("SELECT something FROM user_table")
+      val results: ResultSet = statement.executeQuery()
   }
 
 
